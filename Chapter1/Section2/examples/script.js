8fc7307227ae18cdbd5f034056bc8117cc03095e
Brett Flanders <tourn171@gmail.com>
2015-01-30 04:55:49 +0000

    refactored code cleaner now

--- Chapter1/Section2/examples/script.js
+++ Chapter1/Section2/examples/script.js
@@ -8,7 +8,11 @@ var canvas,
     gre = getId('green'),
     alp = getId('alpha');    
     
+// Extending funcionality to image object
+    
 Image.prototype.filtered = function(r,g,b,a){
+    // Draws image object to canvas and filters to 
+    // input colors
     try{        
         ctx.drawImage(this, 100, 50);
         var myImg = ctx.getImageData(100, 50, 640, 480);
@@ -27,15 +31,22 @@ Image.prototype.filtered = function(r,g,b,a){
     }
 }  
 
+// Helper Functions
+
+
 function getId(id){
+    // Returns DOM object with id
     return document.getElementById(id);
 }
 
 function setValue(obj, amount){
+    // Sets form values
+    // Used to set values on init
     obj.value = amount;
 }
 
 function update(e){
+    // eventHandler for input 
     e.preventDefault;
     var r = parseInt(red.value);
     var g = parseInt(gre.value);
@@ -47,28 +58,36 @@ function update(e){
 
 (function init(){
     
+    // Initialize canvas and set attributes
     canvas = getId('canvas');
     ctx = canvas.getContext('2d');
     canvas.width = W;
     canvas.height = H;
     
-    setValue(red,25);
-    setValue(blu,-200);
-    setValue(gre, -200);
-    setValue(alp, 0);
+    // Initialize color values
+    var r = 25;
+    var g = -200;
+    var b = -200;
+    var a = 0;
+    
+    
+    // initialize form data setting default
+    // values and adding event listeners
+    setValue(red, r);
+    setValue(blu, g);
+    setValue(gre, b);
+    setValue(alp, a);
     red.addEventListener('change', update, false);
     gre.addEventListener('change', update, false);
     blu.addEventListener('change', update, false);
     alp.addEventListener('change', update, false);
-    var r = parseInt(red.value);
-    var g = parseInt(gre.value);
-    var b = parseInt(blu.value);
-    var a = parseInt(alp.value);
     
+
     
+    // Initialize our image set the source
+    // once loaded filter the image.
     ocean = new Image();
     ocean.src = "../images/ocean.jpg";
-
     ocean.onload = ocean.filtered(r, g, b, a);
     
 })();
